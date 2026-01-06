b1b6b10317af120fe5dd36ffe3e9134a4cdb95c1
dltmdrb667 <dltmdrb667@naver.com>
2026-01-07 06:22:40 +0900

    css Typhography 다루기

---
+++ [다운로드]+Tailwind+CSS(v3.4.1)+이론+및+실전+예제+완성본/[다운로드] 섹션 1. [Tailwind CSS 핵심 이론] 기본 요소 다루기/01) Tailwind CSS 시작하기(연습폴더)/01-3)Typography (Font-family 웹폰트)/script/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Noto Sans KR", "sans-serif"],
+        single: ["Single Day", "cursive"],
+        blackhan: ["Black Han Sans", "sans-serif"],
+        yclover: ["YClover"],
+      },
+    },
+  },
+};
---
+++ [다운로드]+Tailwind+CSS(v3.4.1)+이론+및+실전+예제+완성본/[다운로드] 섹션 1. [Tailwind CSS 핵심 이론] 기본 요소 다루기/02) Tailwind CSS에서 Font-family(웹 폰트)/script/tailwind.config.js
@@ -0,0 +1,14 @@
+//tailwind.config.js
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Noto Sans KR', 'Arial', 'sans-serif'],
+        'single': ["Single Day", 'cursive'],
+        'blackhan': ["Black Han Sans", 'sans-serif'],
+        'montserrat': ["Montserrat", 'sans-serif'],
+        'yclover': ['YClover-Bold']
+      }
+    }
+  }
+}
